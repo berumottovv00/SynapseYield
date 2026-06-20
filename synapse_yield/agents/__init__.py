fa31bccb029f2b2f536f01f787041339b73d5a03
shiyu <berumottovv@gmail.com>
2026-06-20 14:22:45 +0800

    删掉无用代码

--- synapse_yield/agents/__init__.py
+++ synapse_yield/agents/__init__.py
@@ -1,18 +1,12 @@
 """LangGraph Agent 编排层。"""
 
-from synapse_yield.agents.graph import AgentGraphConfig, AgentGraphState, TradingAgentGraph
-from synapse_yield.agents.llm_provider import (
-    OpenAITradeProposalProvider,
-    TradeProposalProvider,
-)
 from synapse_yield.agents.harness_agent import HarnessAgentGraph
+from synapse_yield.agents.llm_provider import OpenAITradeProposalProvider, TradeProposalProvider
 from synapse_yield.agents.strategy_agent import StrategyAgent
 from synapse_yield.domain.schemas import StockPick, StockSelectionResult
 from synapse_yield.market.history import CandlestickBar, HistoryFetcher, LongbridgeHistoryFetcher
 
 __all__ = [
-    "AgentGraphConfig",
-    "AgentGraphState",
     "HarnessAgentGraph",
     "StrategyAgent",
     "CandlestickBar",
@@ -22,5 +16,4 @@ __all__ = [
     "StockPick",
     "StockSelectionResult",
     "TradeProposalProvider",
-    "TradingAgentGraph",
 ]
